291838887570b31512c6bd90e1d9130919d928b2
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 21:04:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class RomanticSet
+{
+    public string CreateBouquet() => "Букет троянд ";
+    public string CreateGift() => "Цукерки ";
+}
+
+class ClassicSet
+{
+    public string CreateBouquet() => "Букет тюльпанів ";
+    public string CreateGift() => "Листівка ";
+}
+
+class Program
+{
+    static void Main()
+    {
+        var romantic = new RomanticSet();
+        var classic = new ClassicSet();
+
+        Console.WriteLine(romantic.CreateBouquet() + " + " + romantic.CreateGift());
+        Console.WriteLine(classic.CreateBouquet() + " + " + classic.CreateGift());
+    }
+}
+
+
+
+
